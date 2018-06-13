9aef60e264ed4ad5c1d4dffd02df30280a617f0e
bpc50 <38820740+bpc50@users.noreply.github.com>
2018-06-12 21:56:52 -0400

    6/12 updates

--- 02_numeral-systems/03_hexadecimal/main.go
+++ 02_numeral-systems/03_hexadecimal/main.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("%d - %b - %x \n", 42, 42, 42)
+	//	fmt.Printf("%d - %b - %x \n", 42, 42, 42)	
+	// %d = decimal   %b = binary   %x = hexadecimal 
+	// \n = next line command
+	// the three 42's are progressively applied to each, so, the first 42 is assigned to the %d, the second to %b, etc
+	
+	//fmt.Printf("%d - %b - %#x \n", 42, 42, 42)
+	//fmt.Printf("%d - %b - %#X \n", 42, 42, 42)
+	fmt.Printf("%d \t %b \t %#X \n", 42, 42, 42)
 }
