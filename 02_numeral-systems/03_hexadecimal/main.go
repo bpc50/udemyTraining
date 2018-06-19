811284cd51df60503d8eaac1b67e6adb4d748c58
bpc50 <38820740+bpc50@users.noreply.github.com>
2018-06-18 21:05:34 -0400

    6/18

--- 02_numeral-systems/03_hexadecimal/main.go
+++ 02_numeral-systems/03_hexadecimal/main.go
@@ -7,6 +7,7 @@ func main() {
 	// %d = decimal   %b = binary   %x = hexadecimal
 	// \n = next line command
 	// the three 42's are progressively applied to each, so, the first 42 is assigned to the %d, the second to %b, etc
+	//
 
 	//fmt.Printf("%d - %b - %#x \n", 42, 42, 42)
 	//fmt.Printf("%d - %b - %#X \n", 42, 42, 42)
--- 02_numeral-systems/04_loop/main.go
+++ 02_numeral-systems/04_loop/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i < 200; i++ {
+	for i := 5; i < 50000; i=i+5 {	//instead of i++, i=i+1 means the same thing.
 		fmt.Printf("%d \t %b \t %x \n", i, i, i)
 	}
 }
