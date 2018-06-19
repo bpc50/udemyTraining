f915f7d388711c0f7331692de1ebb84a867abfca
bpc50 <38820740+bpc50@users.noreply.github.com>
2018-06-18 21:38:53 -0400

    6/18 part 2 hardcoding git push in terminal, not using VS

--- 02_numeral-systems/04_loop/main.go
+++ 02_numeral-systems/04_loop/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	for i := 5; i < 50000; i=i+5 {	//instead of i++, i=i+1 means the same thing.
-		fmt.Printf("%d \t %b \t %x \n", i, i, i)
+	for i := 65; i < 123; i=i+1 {	//instead of i++, i=i+1 means the same thing.
+		fmt.Printf("%d \t %b \t %x \t %q \n", i, i, i, i)
 	}
 }
